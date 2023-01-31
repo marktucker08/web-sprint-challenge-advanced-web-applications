7f66bce944d865b65b77a741a02c5558e1688723
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2023-01-30 19:20:37 -0700

    corrected conditional logic for redirect of protected route to articles

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -8,7 +8,7 @@ export default function Articles(props) {
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
-  if (localStorage.getItem('token') === '') {
+  if (!localStorage.getItem('token')) {
     console.log("No Token Found!!!")
     return (
       <Navigate to='/' />
